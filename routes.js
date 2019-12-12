87bfc89b2ed7c69459e042789d32363641b13d5a
kostyan.malinin <kostyan.malinin@yandex.ru>
2019-12-12 19:57:43 +0400

    lab7

--- routes.js
+++ routes.js
@@ -1,35 +1,37 @@
-//cd C:\"Program Files"\PostgreSQL\11\bin
-//pg_ctl -D /"Program Files"/PostgreSQL/11/data start
-//npm run dev
-
 let jwt = require('jsonwebtoken');
 const secretKey = "myTestSecretKey";
+const fileLoad = require('./files');
+const path = require('path');
+const fs = require('fs');
+
 
 module.exports = function(app, db) {
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, descriptionization");
-        next();
-    }),
-    app.get('/testdb', async (req, res) => {
-        res.send(`DB url ${process.env.DATABASE_url}`);
-    }),
-
-    app.post('/category', async (req, res) => {
-        let object = convertToObj(req.body);
-        if (object.pageName == "admin"){
-            jwt.verify(object.token, secretKey, async function(err, decoded){
-                if(err) return res.send(false);
-                if(!decoded.isAdmin) return res.send(false);
-                let products = await db.Models.Product.findAll();
-                res.send(products);
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Athorization");
+        if (['/category', '/category/create', '/category/update', '/category/delete'].includes(req.originalUrl)) {
+            let object = convertToObj(req.body);
+            if (req.originalUrl === '/category' && object.pageName !== "admin") {
+                return next();
+            }
+            jwt.verify(object.token, secretKey, async function(err, decoded) {
+                if (err) return res.send(false);
+                if (!decoded.isAdmin) return res.send(false);
+                next();
             });
         }
-        else{
-            let products = await db.Models.Product.findAll();
-            res.send(products);
+        else {
+            next();
         }
+    });
+    
+    app.get('/testdb', async (req, res) => {
+        res.send(`DB url ${process.env.DATABASE_URL}`);
+    });
 
+    app.post('/category', async (req, res) => {       
+        let products = await db.Models.Product.findAll();
+        res.send(products);
     });
 
     app.post('/login', async (req, res) => {
@@ -41,34 +43,29 @@ module.exports = function(app, db) {
             }
         });
         if (user != null) {
-            user.token = jwt.sign({ login: object.login, isAdmin: user.isAdmin }, secretKey);
-            await user.save();
+            let token = jwt.sign({ login: object.login, isAdmin: user.isAdmin }, secretKey);
             res.send({
                 login: user.login,
-                token: user.token
+                isAdmin: user.isAdmin,
+                token: token
             });
         }
         else {
             res.send(false);
-        }
+        } 
     });
 
     app.post('/category/create', async (req, res) => {
         let object = convertToObj(req.body);
-        jwt.verify(object.token, secretKey, async function(err,decoded){
-            if(err) return res.send(false);
-            if(!decoded.isAdmin) return res.send(false);
-            object = object.data;
-
-            if ( object.category == null || object.price == null ||  object.description == null || object.url == null) return res.send(false);
-            let product = await db.Models.Product.create({
-                category: object.category,
-                price: object.price,   
-                description: object.description,
-                url: object.url,
-            });
-            res.send(product);
+        object = object.data;
+        if (object.category == null || object.price == null || object.description == null || object.url == null) return res.send(false);
+        let product = await db.Models.Product.create({
+            category: object.category,
+            price: object.price,
+            description: object.description,
+            url: object.url
         });
+        res.send(product);
     });
 
     app.post('/reg', async (req, res) => {
@@ -83,12 +80,15 @@ module.exports = function(app, db) {
                 login: object.login,
                 password: object.password,
                 isAdmin: false,
+            });
+            res.send({
+                login: newUser.login,
+                isAdmin: newUser.isAdmin,
                 token: jwt.sign({
                     login: object.login,
                     isAdmin: false
                 }, secretKey)
             });
-            res.send(true);
         }
         else {
             res.send(false);
@@ -97,50 +97,61 @@ module.exports = function(app, db) {
 
     app.post('/category/update', async (req, res) => {
         let object = convertToObj(req.body);
-
-        jwt.verify(object.token, secretKey, async function(err,decoded){
-            if(err) return res.send(false);
-            if(!decoded.isAdmin) return res.send(false);
-            object = object.data;
-            let id = parseInt(object.id);
-           
-            if (isNaN(id)  || object.category == null ||  object.price == null || object.description == null || object.url == null) return res.send(false);
-            let products = await db.Models.Product.update({
-                category: object.category,
-                price: object.price,
-                description: object.description,
-                url: object.url
-                
-            }, {
-                where: {
-                    id: id,
-                } 
-            });
-            res.send(object);
+        object = object.data;
+        let id = parseInt(object.id);
+        console.log(object);
+        if (isNaN(id) || object.category == null || object.price == null || object.description == null || object.url == null) return res.send(false);
+        let products = await db.Models.Product.update({
+            category: object.category,
+            price: object.price,
+            description: object.description,
+            url: object.url
+        }, {
+            where: {
+                id: id,
+            } 
         });
+        res.send(object);
     });
 
     app.post('/category/delete', async (req, res) => {
         let object = convertToObj(req.body);
+        object = object.data;
+        let id = parseInt(object.id);
 
-        jwt.verify(object.token, secretKey, async function(err,decoded){
-            if(err) return res.send(false);
-            if(!decoded.isAdmin) return res.send(false);
-            object = object.data;
-            let id = parseInt(object.id);
- 
-            if (isNaN(id)) return res.send(false);
-            await db.Models.Product.destroy({
-                where: {
-                    id: id,
-                } 
-            });
+        if (isNaN(id)) return res.send(false);
+        await db.Models.Product.destroy({
+            where: {
+                id: id,
+            } 
+        });
+        res.send(true);
+    });
+
+    app.post('/upload', fileLoad.upload.single('file'), (req, res) => {
+        const { file } = req;
+
+        if(!file){
+            console.log('File null');
+            return res.send(false);
+        }
+        
+        dropbox({
+            resource: 'files/upload',
+            parameters:{
+                path: '/' + file.originalname
+            },
+            readStream: fs.createReadStream(path.resolve(fileLoad.PATH, file.originalname))
+        }, (err, result, response) =>{
+            if (err) return console.log(err);
+    
+            console.log('uploaded dropbox');
             res.send(true);
         });
     });
+    
+  
 };
 let convertToObj = function(obj) {
-    for (const key in obj) {
-        return JSON.parse(key);
-    }
+    return JSON.parse(obj.data);
 }
