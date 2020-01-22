461a791a28ed759ca905e8efeaae5e5eefedf3f6
kostyan.malinin <kostyan.malinin@yandex.ru>
2020-01-22 23:55:03 +0400

    port

--- routes.js
+++ routes.js
@@ -11,7 +11,9 @@ const WebSocketClient = require('websocket').client;
 
 module.exports = function (app, db) {
     app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", /*"https://warehouse73.herokuapp.com"*/"http://localhost:4200");
+        res.header("Access-Control-Allow-Origin", "https://warehouse73.herokuapp.com");
+        //res.header("Access-Control-Allow-Origin",  "http://localhost:4200");
+       
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         if (['/category/create', '/category/update', '/category/delete'].includes(req.originalUrl)) {
             let object = convertToObj(req.body);
